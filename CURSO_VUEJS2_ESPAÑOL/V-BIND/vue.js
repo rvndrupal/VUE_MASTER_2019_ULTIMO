1398dcd248b89996724fb9d24f87d38f33571bfc
rvndrupal <rodrigodrupal1@gmail.com>
2019-10-08 16:28:40 -0500

    V-model V-text

---
+++ CURSO_VUEJS2_ESPAÑOL/V-BIND/vue.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+
+    el:'#app',
+
+    data: {
+        src:'http://lorempixel.com/400/200/'
+    },
+  
+
+})
---
+++ CURSO_VUEJS2_ESPAÑOL/V-TEXT/vue.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+
+    el:'#app',
+
+    data: {
+        nombre: 'Rodrigo Villanueva Nieto'
+    },
+  
+
+})
